d67bafa4ea1977156ba61974a8d7b5f84b5ef6dc
Chen Jiang <chen.jiang.1206@gmail.com>
2023-03-21 19:39:53 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+function writeCards(names, adj) {
+    let cards = []
+    for (let name of names) {
+        cards.push(`Thank you, ${name}, for the wonderful ${adj} gift!`)
+    }
+    return cards;
+}
+
+//SYNTAX -- for of 
+
+function countDown(num) {
+    let i = num
+    while (i >= 0) {
+        console.log(i);
+        i--;
+    }
+}
+
